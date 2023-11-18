632281eb18e4e4a6e3ccb5a098c91d221f565d7e
bobsport33 <bob62kelly@gmail.com>
2023-11-17 23:18:41 -0600

    add more module config

--- src/components/secondaryHero/Index.tsx
+++ src/components/secondaryHero/Index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import Image from "next/image";
 
 import { SecondaryHeroProps } from "@/types";
+import { colors } from "@/styles/variables";
+import { EyebrowStyles, P_LargeStyles } from "@/styles/type";
 
-const SecondaryHeroContainer = styled.section``;
+const SecondaryHeroContainer = styled.section`
+    .hero {
+        &__text-container {
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+            padding: 20px;
+        }
+
+        &__eyebrow {
+            ${EyebrowStyles};
+            color: ${colors.neutral900};
+        }
+
+        &__heading {
+            color: ${colors.neutral900};
+        }
+
+        &__description {
+            ${P_LargeStyles};
+            color: ${colors.neutral900};
+        }
+    }
+`;
 
 const SecondaryHero = ({
     eyebrow,
@@ -13,9 +39,18 @@ const SecondaryHero = ({
 }: SecondaryHeroProps) => {
     return (
         <SecondaryHeroContainer>
-            <p className="hero__eyebrow">{eyebrow}</p>
-            <h1 className="hero__heading">{title}</h1>
-            <p className="hero__description">{description}</p>
+            <div className="hero__text-container">
+                <p className="hero__eyebrow">{eyebrow}</p>
+                <h1 className="hero__heading">{title}</h1>
+                <p className="hero__description">{description}</p>
+            </div>
+            <Image
+                className="hero__image"
+                src={image.imageUrl}
+                alt={image.imageAlt}
+                height={800}
+                width={1440}
+            />
         </SecondaryHeroContainer>
     );
 };
--- src/components/sideBySideContent/Index.tsx
+++ src/components/sideBySideContent/Index.tsx
@@ -1,9 +1,44 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { motion } from "framer-motion";
 
 import { SideBySideProps } from "@/types";
+import Image from "next/image";
+import { Container } from "@/styles/mixins";
+import { P_MediumStyles } from "@/styles/type";
 
-const SideBySideContainer = styled.section``;
+interface SideBySideContainerProps {
+    $imageOnRight?: boolean;
+}
+
+const SideBySideContainer = styled.section<SideBySideContainerProps>`
+    ${Container};
+    display: flex;
+    gap: 50px;
+    justify-content: space-around;
+    align-items: center;
+
+    ${({ $imageOnRight }) => {
+        if (!$imageOnRight) {
+            return css`
+                flex-direction: row-reverse;
+            `;
+        }
+    }}
+
+    .content {
+        &__text-container {
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+        }
+        &__heading {
+        }
+        &__description {
+            ${P_MediumStyles}
+        }
+    }
+`;
 
 const SideBySideContent = ({
     image,
@@ -11,8 +46,40 @@ const SideBySideContent = ({
     description,
     imageOnRight,
 }: SideBySideProps) => {
-    console.log(title, description);
-    return <SideBySideContainer></SideBySideContainer>;
+    const slideIn = {
+        initial: { x: `${imageOnRight ? "-100%" : "100%"}`, opacity: 0 },
+        whileInView: { x: "0", opacity: 1 },
+        transition: { duration: 0.8 },
+        viewport: { once: true },
+    };
+
+    const slideInOpposite = {
+        initial: { x: `${imageOnRight ? "100%" : "-100%"}`, opacity: 0 },
+        whileInView: { x: "0", opacity: 1 },
+        transition: { duration: 0.8 },
+        viewport: { once: true },
+    };
+
+    return (
+        <SideBySideContainer $imageOnRight={imageOnRight}>
+            <motion.div className="content__text-container" {...slideIn}>
+                <h1 className="content__heading">{title}</h1>
+                <p className="content__description">{description}</p>
+            </motion.div>
+            <motion.div
+                className="content__image-container"
+                {...slideInOpposite}
+            >
+                <Image
+                    className="content__image"
+                    src={image.imageUrl}
+                    alt={image.imageAlt}
+                    height={600}
+                    width={600}
+                />
+            </motion.div>
+        </SideBySideContainer>
+    );
 };
 
 export default SideBySideContent;
